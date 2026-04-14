135ff123061f183a02d78a16aa05214773cdd956
smshubham2005 <shubhamhmath7@gmail.com>
2026-04-14 10:17:51 +0530

    Java app changed

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,20 +3,32 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
+public class App {
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Read system property
+        String headless = System.getProperty("headless");
+
+        if ("true".equals(headless)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+        
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
     }
 }
