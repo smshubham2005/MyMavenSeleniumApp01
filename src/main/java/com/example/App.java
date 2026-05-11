8cf748cf1e711d78cd5243bfc4c7522f0061e579
smshubham2005 <shubhamhmath7@gmail.com>
2026-05-11 06:13:32 +0530

    Enhance ChromeOptions for headless execution
    Added additional ChromeOptions for headless mode.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
+
     public static void main(String[] args) {
 
         ChromeOptions options = new ChromeOptions();
@@ -14,15 +15,25 @@ public class App {
         String headless = System.getProperty("headless");
 
         if ("true".equals(headless)) {
+
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
+
+            // SSL / Jenkins VM fixes
+            options.addArguments("--ignore-certificate-errors");
+            options.addArguments("--allow-insecure-localhost");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--remote-allow-origins=*");
+
+            // Accept insecure certificates
+            options.setAcceptInsecureCerts(true);
         }
 
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
-        
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
